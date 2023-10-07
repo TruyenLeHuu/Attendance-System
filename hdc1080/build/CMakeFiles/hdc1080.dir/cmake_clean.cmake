8ed1e20fed2029cf8817c7b256a833e486853770
truyen <20522094@gm.uit.edu.vn>
2023-10-07 22:52:28 +0700

    add code hdc1080

---
+++ hdc1080/build/CMakeFiles/hdc1080.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hdc1080.dir/src/main.cpp.o"
+  "hdc1080"
+  "hdc1080.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hdc1080.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
